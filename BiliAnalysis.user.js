b6f248329b5fef43734c0df7cdb6109205ff142c
teatube <teatube@qq.com>
2023-08-11 19:14:12 +0800

    # 基于原脚本的调整: ## 视频 - 按钮变大 - 支持了打开有活动（festival）页面当中，地址栏有bvid的视频的解析     > 在页面内点击切换视频时，因为地址栏并没有改变，所以不会动     > 直接打开活动页面时，地址栏没有bvid的值，所以不会动     > 可能日后会随心情完善 ## 直播 - 打开无后缀链接时也可以解析 - 展示所有可能的解析（点击展开/收回） - 有海报的直播间也可以正常显示按钮

--- BiliAnalysis.user.js
+++ BiliAnalysis.user.js
@@ -1,68 +1,74 @@
 // ==UserScript==
-// @name         BiliBili本地解析(Miro)
+// @name         BiliBili本地解析(Miro)·茶叶改
 // @namespace    https://bbs.tampermonkey.net.cn/
-// @version      0.1.3
-// @description  try to take over the world!
-// @author       Miro(https://vrchat.com/home/user/usr_20b8e0e4-9e16-406a-a61d-8a627ec1a2e3)
-// @match        https://www.bilibili.com/video*
-// @downloadURL  https://raw.githubusercontent.com/529565622/BiliAnalysis/main/BiliAnalysis.user.js
-// @updateURL    https://raw.githubusercontent.com/529565622/BiliAnalysis/main/BiliAnalysis.user.js
+// @version      0.1.2
+// @description  基于Miro_355的b站解析脚本(https://www.bilibili.com/video/BV1AP411x7YW/)
+// @author       Miro(https://vrchat.com/home/user/usr_20b8e0e4-9e16-406a-a61d-8a627ec1a2e3),-茶叶子-(https://vrchat.com/home/user/usr_411b8ebe-a9a9-4987-a4c8-5e17c898a311)
+// @downloadURL  https://raw.githubusercontent.com/teatube/BiliAnalysis/main/BiliAnalysis.user.js
+// @updateURL    https://raw.githubusercontent.com/teatube/BiliAnalysis/main/BiliAnalysis.user.js
+// @match        https://www.bilibili.com/video/*
+// @match        https://www.bilibili.com/festival/*
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
 // @run-at       document-idle
-// @require           https://lf26-cdn-tos.bytecdntp.com/cdn/expire-1-M/jquery/3.2.1/jquery.min.js
 // ==/UserScript==
-//20230405 修复解析1080p(需已登陆)
-//20230626 修复加载慢导致无法添加按钮
-//20230811 添加右下角解析按钮 加快按钮出现速度
+
+
 (function () {
     'use strict';
-    var button = document.createElement("button")
-    button.textContent = "本地解析"
-    button.style.width = "80px"
-    button.style.align = "center"
-    button.style.color = "#FFFFFF"
-    button.style.background = "#00AEEC"
-    button.style.border = "1px solid #F1F2F3"
-    button.style.borderRadius = "6px"
-    button.style.fontSize = '14px'
-    button.addEventListener("click", clickBotton)
-    var BiliAnalysisbutton = `<button id="BiliAnalysis" style="width: 40px; color: rgb(255, 255, 255); background: rgb(0, 174, 236); border: 1px solid rgb(241, 242, 243); border-radius: 6px; font-size: 14px;top:800px;right:0px;position:fixed;">本地解析</button>`;
-    $("body").append(BiliAnalysisbutton)
-    document.getElementById('BiliAnalysis').addEventListener('click', clickBotton)
+    var button = document.createElement("button");
+    button.textContent = "本地解析";
+    button.style.width = "80px";
+    button.style.align = "center";
+    button.style.color = "#FFFFFF";
+    button.style.background = "#00AEEC";
+    button.style.border = "1px solid #F1F2F3";
+    button.style.borderRadius = "6px";
+    button.style.fontSize = '14px';
+    button.style.marginLeft = '8px';
+    button.style.padding = "6px";
+    button.addEventListener("click", clickButton);
     setTimeout(function () {
-        var like_comment = document.getElementsByClassName('left-entry')[0]
-        like_comment.appendChild(button)
-    }, 5000)
-    setTimeout(function () {
-        var like_comment = document.getElementsByClassName('left-entry')[0]
-        like_comment.appendChild(button)
-    }, 10000)
-    function clickBotton() {
-        var url = window.location.href
-        var BV = /(?=BV).*?(?=\?|\/)/
-        var P = /(?<=p=).*?(?=&vd)/
-        var BV1 = url.match(BV)
-        var P1 = url.match(P)
+        var likeComment = document.getElementsByClassName('left-entry')[0];
+        if (!likeComment){
+           likeComment = document.getElementsByClassName('nav-link-ul')[0];
+        }
+        likeComment.appendChild(button);
+    }, 5000);
+
+    function clickButton() {
+        var url = window.location.href;
+        var urlParams = new URLSearchParams(window.location.search);
+        var bvid = urlParams.get('bvid');
+        if (!bvid) {
+            var matchBv = url.match(/(?<=video\/).*?(?=\/|$)/);
+            if (!matchBv) {
+                console.error("BV号未找到");
+                return;
+            }
+            bvid = matchBv[0];
+        }
+        var P = /(?<=p=).*?(?=&vd)/;
+        var P1 = url.match(P);
         if (P1 == null) {
-            P1 = 1
+            P1 = 1;
         }
         var httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET', 'https://api.bilibili.com/x/player/pagelist?bvid=' + BV1, true);
+        httpRequest.open('GET', 'https://api.bilibili.com/x/player/pagelist?bvid=' + bvid, true);
         httpRequest.send();
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
                 var json = JSON.parse(httpRequest.responseText);
-                var cid = json.data[P1 - 1].cid
+                var cid = json.data[P1 - 1].cid;
                 console.log(json.data[P1 - 1].cid);
                 var httpRequest1 = new XMLHttpRequest();
-                httpRequest1.open('GET', 'https://api.bilibili.com/x/player/playurl?bvid=' + BV1 + '&cid=' + cid + '&qn=116&type=&otype=json&platform=html5&high_quality=1', true);
+                httpRequest1.open('GET', 'https://api.bilibili.com/x/player/playurl?bvid=' + bvid + '&cid=' + cid + '&qn=116&type=&otype=json&platform=html5&high_quality=1', true);
                 httpRequest1.withCredentials = true;
                 httpRequest1.send();
                 httpRequest1.onreadystatechange = function () {
                     if (httpRequest1.readyState == 4 && httpRequest1.status == 200) {
                         var json = JSON.parse(httpRequest1.responseText);
-                        navigator.clipboard.writeText(json.data.durl[0].url).catch(e => console.error(e))
+                        navigator.clipboard.writeText(json.data.durl[0].url).catch(e => console.error(e));
                         console.log(json.data.durl[0].url);
                     }
                 };
@@ -75,10 +81,8 @@
             highlight: true,
             silent: false,
             timeout: 10000,
-            onclick: function () {
-            },
-            ondone() {
-            }
-        })
+            onclick: function () {},
+            ondone() {}
+        });
     }
 })();
--- BiliRoomAnalysis.user.js
+++ BiliRoomAnalysis.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
-// @name         BiliBili直播间本地解析(Miro)
+// @name         BiliBili直播间本地解析(Miro)·茶叶改
 // @namespace    https://bbs.tampermonkey.net.cn/
-// @version      0.1.0
-// @description  try to take over the world!
-// @author       Miro(https://vrchat.com/home/user/usr_20b8e0e4-9e16-406a-a61d-8a627ec1a2e3)
+// @version      0.1.2
+// @description  基于Miro_355的b站解析脚本(https://www.bilibili.com/video/BV1AP411x7YW/)
+// @author       Miro(https://vrchat.com/home/user/usr_20b8e0e4-9e16-406a-a61d-8a627ec1a2e3)、-茶叶子-(https://vrchat.com/home/user/usr_411b8ebe-a9a9-4987-a4c8-5e17c898a311)
 // @match        https://live.bilibili.com/*
-// @downloadURL  https://raw.githubusercontent.com/529565622/BiliAnalysis/main/BiliRoomAnalysis.user.js
-// @updateURL    https://raw.githubusercontent.com/529565622/BiliAnalysis/main/BiliRoomAnalysis.user.js
+// @downloadURL  https://raw.githubusercontent.com/teatube/BiliAnalysis/main/BiliRoomAnalysis.user.js
+// @updateURL    https://raw.githubusercontent.com/teatube/BiliAnalysis/main/BiliRoomAnalysis.user.js
 // @grant        GM_xmlhttpRequest
 // @grant        GM_notification
 // @run-at       document-idle
@@ -14,55 +14,130 @@
 
 (function () {
     'use strict';
-    var button = document.createElement("button")
-    button.textContent = "本地解析"
-    button.style.width = "80px"
-    button.style.align = "center"
-    button.style.color = "#FFFFFF"
-    button.style.background = "#00AEEC"
-    button.style.border = "1px solid #F1F2F3"
-    button.style.borderRadius = "6px"
-    button.style.fontSize = '14px'
-    button.addEventListener("click", clickBotton)
-    setTimeout(function () {
-        var like_comment = document.getElementsByClassName('flex-block')[0]
-        like_comment.appendChild(button)
-    }, 5000)
-        setTimeout(function () {
-        var like_comment = document.getElementsByClassName('flex-block')[0]
-        like_comment.appendChild(button)
-    }, 10000)
-    function clickBotton() {
-        var url = window.location.href
-        var Roomid=/(?<=com\/).*?(?=\?)/
-        var Roomid1 = url.match(Roomid)
+
+    var buttonsContainer;
+
+    function createButton(text, clickHandler, isExpanded) {
+        var button = document.createElement("button");
+        button.textContent = text;
+        // button.style.width = "120px";
+        button.style.align = "center";
+        button.style.color = "#FFFFFF";
+        button.style.background = "#00AEEC";
+        button.style.border = "1px solid #F1F2F3";
+        button.style.borderRadius = "6px";
+        button.style.fontSize = '14px';
+        button.style.padding = "8px";
+        button.style.marginLeft = '8px';
+
+        button.addEventListener("click", function () {
+            clickHandler();
+            if (isExpanded) {
+                button.textContent = "展开解析";
+            } else {
+                button.textContent = "收回解析";
+            }
+            isExpanded = !isExpanded;
+        });
+        return button;
+    }
+
+    function toggleButtonsVisibility() {
+        if (buttonsContainer.style.display === "none") {
+            buttonsContainer.style.display = "block";
+        } else {
+            buttonsContainer.style.display = "none";
+        }
+    }
+
+function addButtonContainer(json) {
+    var likeComment = document.getElementsByClassName('flex-block')[0];
+    if (!likeComment){
+        likeComment = document.getElementsByClassName('left-entry')[0];
+    }
+
+    buttonsContainer = document.createElement("div");
+    buttonsContainer.style.display = "none"; // 初始状态为已折叠
+
+    var toggleButton = createButton("展开解析", toggleButtonsVisibility, false);
+    likeComment.appendChild(toggleButton);
+    function toggleButtonsVisibility() {
+        if (buttonsContainer.style.display === "none") {
+            buttonsContainer.style.display = "block";
+            toggleButton.textContent = "收回解析"; // 更新按钮文字
+        } else {
+            buttonsContainer.style.display = "none";
+            toggleButton.textContent = "展开解析"; // 更新按钮文字
+        }
+    }
+
+    var hlsStream = json.data.playurl_info.playurl.stream.find(function (stream) {
+        return stream.protocol_name === 'http_hls';
+    });
+    var formatCount = {}; // 用于记录每个 format_name 的计数
+
+    if (hlsStream) {
+            for (var formatIndex = 0; formatIndex < hlsStream.format.length; formatIndex++) {
+                var formatName = hlsStream.format[formatIndex].format_name;
+
+
+                for (var codecIndex = 0; codecIndex < hlsStream.format[formatIndex].codec.length; codecIndex++) {
+                    var baseurl = hlsStream.format[formatIndex].codec[codecIndex].base_url;
+                    var urlInfoArray = hlsStream.format[formatIndex].codec[codecIndex].url_info;
+
+                    for (var urlIndex = 0; urlIndex < urlInfoArray.length; urlIndex++) {
+                        (function(host, extra) {
+                            var roomurl = host + baseurl + extra;
+                            if (!formatCount[formatName]) {
+                                formatCount[formatName] = 1;
+                            } else {
+                                formatCount[formatName]++;
+                            }
+                            var buttonLabel = formatName + "解析" +formatCount[formatName];
+
+                            var button = createButton(buttonLabel, function () {
+                                navigator.clipboard.writeText(roomurl).catch(e => console.error(e));
+                                GM_notification({
+                                    title: "解析成功",
+                                    image: "https://i0.hdslb.com/bfs/archive/86848c76a76fe46d84d6ef1ab735d9398ed3ee8e.png",
+                                    text: "解析成功",
+                                    highlight: true,
+                                    silent: false,
+                                    timeout: 10000,
+                                    onclick: function () {},
+                                    ondone() {}
+                                });
+                            });
+
+                            buttonsContainer.appendChild(button);
+                        })(urlInfoArray[urlIndex].host, urlInfoArray[urlIndex].extra);
+                    }
+                }
+            }
+    } else {
+        // 在界面上显示未找到 http_hls 流的提示
+        var errorMessage = document.createElement("div");
+        errorMessage.textContent = "未找到 http_hls 流。";
+        buttonsContainer.appendChild(errorMessage);
+    }
+
+    likeComment.appendChild(buttonsContainer);
+}
+
+    function fetchAndAddButtons() {
+        var url = window.location.href;
+        var Roomid = /(?<=com\/).*?(?=\?|$)/;
+        var Roomid1 = url.match(Roomid);
         var httpRequest = new XMLHttpRequest();
-        httpRequest.open('GET', 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id='+Roomid1+'&protocol=0,1&format=0,1,2&codec=0,1&qn=10000&platform=web&ptype=8&dolby=5&panorama=1', true);
+        httpRequest.open('GET', 'https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id=' + Roomid1 + '&protocol=0,1&format=0,1,2&codec=0,1&qn=10000&platform=web&ptype=8&dolby=5&panorama=1', true);
         httpRequest.send();
         httpRequest.onreadystatechange = function () {
             if (httpRequest.readyState == 4 && httpRequest.status == 200) {
                 var json = JSON.parse(httpRequest.responseText);
-                var host = json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].host
-                console.log(host);
-                var baseurl=json.data.playurl_info.playurl.stream[1].format[1].codec[0].base_url
-                console.log(baseurl);
-                var extra=json.data.playurl_info.playurl.stream[1].format[1].codec[0].url_info[0].extra
-                console.log(extra);
-                var roomurl=host+baseurl+extra
-                navigator.clipboard.writeText(roomurl).catch(e => console.error(e))
-                GM_notification({
-                    title: "解析成功",
-                    image: "https://i0.hdslb.com/bfs/archive/86848c76a76fe46d84d6ef1ab735d9398ed3ee8e.png",
-                    text: "解析成功",
-                    highlight: true,
-                    silent: false,
-                    timeout: 10000,
-                    onclick: function () {
-                    },
-                    ondone() {
-                    }
-                })
+                addButtonContainer(json);
             }
         };
     }
+
+    setTimeout(fetchAndAddButtons, 5000);
 })();
